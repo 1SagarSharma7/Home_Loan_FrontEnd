c0fdb7d012c52eab4be3e80e4b1a7414016c96fd
SagarSharma <1sagarsharma7@gmail.com>
2022-09-28 16:56:10 +0530

    essence

--- src/app/eli-cal/eli-cal.component.ts
+++ src/app/eli-cal/eli-cal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-eli-cal',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EliCalComponent implements OnInit {
 
-  constructor() { }
+  monthlyIncome !: number;
+  eligibileLoanAmount !: number;
+  val:number=0.6;
+
+  formValue !: FormGroup;
+
+  constructor(private formbuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.formValue = this.formbuilder.group({
+      monthlyIncome : [''],
+    })
+  }
+
+  calculateELI(){
+    this.monthlyIncome = this.formValue.value.monthlyIncome;
+
+    this.calculate();
+  }
+
+  calculate() {
+    this.eligibileLoanAmount = 60 * (this.val * this.monthlyIncome);
   }
 
 }
--- src/app/emi-cal/emi-cal.component.ts
+++ src/app/emi-cal/emi-cal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-emi-cal',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmiCalComponent implements OnInit {
 
-  constructor() { }
+  loanAmount !: number;
+  tenureInMonths !: number;
+  interest !: number;
+  emi !: number;
+
+  formValue !: FormGroup;
+
+  constructor(private formbuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.formValue = this.formbuilder.group({
+      loanAmount : [''],
+      tenureInMonths : [''],
+      interest : [''],
+      emi : ['']
+    })
   }
+
+  calculateEMI(){
+    this.loanAmount = this.formValue.value.loanAmount;
+    this.tenureInMonths = this.formValue.value.tenureInMonths;
+    this.interest = this.formValue.value.interest;
+
+    this.calculate();
+  }
+
+  calculate() {
+    this.interest = (this.loanAmount * (this.interest*0.01))/this.tenureInMonths;
+    this.emi = Number((this.loanAmount /this.tenureInMonths + this.interest).toFixed(2));
+  }
+
+  
 
 }
